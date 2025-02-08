56c4b7620656292e85fe92920b14142552dbc66d
Kaspar Martin Suursalu <kasparsu@gmail.com>
2025-02-08 11:25:45 +0200

    Add nuxt

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script setup>
+import axios from 'axios';
+let posts = ref([]);
+axios.get('http://127.0.0.1:8000/api/posts').then(response => {
+  console.log(response.data.data);
+  posts.value = response.data.data;
+});
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <ul>
+      <li v-for="post in posts">{{ post.title }}</li>
+    </ul>
+  </div>
+</template>
